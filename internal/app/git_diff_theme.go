ba0a5695f28e874ef80b0b4ad476bbf84524c4b2
namest504 <namest504@gmail.com>
2025-11-03 18:07:19 +0900

    feat(diff): Add theme

--- internal/app/git_diff_theme.go
+++ internal/app/git_diff_theme.go
@@ -38,6 +38,30 @@ func (t *DiffTheme) ResetState(gs *GameState) {
 }
 
 func (t *DiffTheme) UpdateScreen(s tcell.Screen, gs *GameState) {
+	state, ok := gs.CustomState.(*DiffThemeState)
+	if !ok {
+		return
+	}
+	s.Clear()
+
+	// 컨텍스트 라인 그리기
+	drawText(s, 0, 0, tcell.StyleDefault.Foreground(tcell.ColorDimGray), "diff --git a/main.go b/main.go")
+	drawText(s, 0, 1, tcell.StyleDefault.Foreground(tcell.ColorDimGray), "--- a/main.go")
+	drawText(s, 0, 2, tcell.StyleDefault.Foreground(tcell.ColorDimGray), "+++ b/main.go")
+	drawText(s, 0, 3, tcell.StyleDefault.Foreground(tcell.ColorBlue), "@@ -1,5 +1,5 @@")
+
+	y := 4
+	for i, line := range state.ContextLines {
+		if i == 2 { // 문장이 들어갈 위치
+			plusStyle := tcell.StyleDefault.Foreground(tcell.ColorGreen)
+			drawText(s, 0, y, plusStyle, "+ "+gs.targetSentence)
+		} else {
+			drawText(s, 0, y, tcell.StyleDefault, " "+line)
+		}
+		y++
+	}
+
+	s.Show()
 }
 
 func (t *DiffTheme) OnTick(gs *GameState) {}
